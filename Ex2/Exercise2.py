9b0314f117a2e5becb2a40a52d06aa9538b17a55
Jim Totland <jimtotland@gmail.com>
2021-02-04 09:06:33 +0100

    got an answer for a)

--- Ex2/Exercise2.py
+++ Ex2/Exercise2.py
@@ -10,8 +10,20 @@ def normalize(a):
     return a / np.sum(a)
 
 
-def forward(T,O,f):
-    return normalize(O @ T.T @ f)
+def forward(Tmatrix,Omatrices,evidence):
+    if not evidence:
+        return np.ones(2)
 
-Omatrix = np.array([[0.75, 0], [0, 0.2]])
+    e = evidence.pop(-1)
+    if e:
+        return normalize(Omatrices[0] @ Tmatrix.T @ forward(Tmatrix, Omatrices, evidence.copy()))
+    else:
+        return normalize(Omatrices[1] @ Tmatrix.T @ forward(Tmatrix, Omatrices, evidence.copy()))
+
+
+Omatrices = [np.array([[0.75, 0], [0, 0.2]]), np.array([[0.25, 0], [0, 0.8]])] # First: B_t = true,  Second: B_t = false
 Tmatrix = np.array([[0.7, 0.3],[0.2, 0.8]])
+evidence = [True, True, False, True, False, True]
+
+print(forward(Tmatrix, Omatrices, evidence))
+
---
+++ Ex2/testing.py
@@ -0,0 +1,6 @@
+import numpy as np
+
+i = np.ones(2)
+i.reshape(2,1)
+
+print(np.shape(i))
