801569ce052be2699183388d42f206beb511bf8c
Jim Totland <jimtotland@gmail.com>
2021-02-15 19:52:11 +0100

    altered the viterbi again. What remains is to remove some stuff

--- Ex2/Exercise2.py
+++ Ex2/Exercise2.py
@@ -2,19 +2,24 @@
 #===========================#
 # Exercise 2: Methods in AI #
 #===========================#
+# Note to reader: I have sticked to the convention that row 0 or index 0 corresponds to the state false, and row 1 
+# or index 1 corresponds to the state false. This can be a bit confusing with reference to the course book by Russel
+# and Norvig.
 
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('seaborn')
 
 
-# b)
+## b)
 def normalize(a):
+    '''Normalize distribution a.'''
     return a / np.sum(a)
 
 
 
 def forward(Tmatrix, Omatrices, evidence):
+    '''Compute a list of forward messages given evidence.'''
     t = len(evidence)
     fList = [np.array([0.5, 0.5])]
     for i in range(t):
@@ -36,16 +41,18 @@ evidence = [1, 1, 0, 1, 0, 1]
 filter = forward(Tmatrix,Omatrices, evidence)
 print("\nFiltering: ")
 print(filter)
+
+# Plotting:
 x = [i for i in range(len(filter))]
 p = [f[1] for f in filter]
-
 plt.plot(x,p, label = "Filtering", linewidth = 3)
 plt.xlabel("Time, $t$")
 plt.ylabel("Probability")
 
-# c)
+## c)
 
 def predict(Tmatrix, initial, tLower, tUpper):
+    '''Compute the predicted distribution of the state variable up until time tUpper'''
     predList = []
     predList.append(initial @ Tmatrix)
     for k in range(tUpper - tLower):
@@ -58,17 +65,15 @@ prediction = predict(Tmatrix, initial, 7, 30)
 print("\n Prediction:")
 print(prediction)
 
+# Plotting:
 y = [p[1] for p in prediction]
 x = [i for i in range(7,31)]
 plt.plot(x,y, label = "Prediction", linewidth = 3)
 
-# This operation is called prediction, beacuse it predicts the distribution og X's states for future time points t > k,
-# for evidence given up until t = k.
-# As t increases, we can see that the the distribution over X apporaches a stationary distribution.
-
-# d)
+## d)
 
 def backward(Tmatrix, Omatrices, evidence):
+    '''Compute a list of the backward messages given evidence.'''
     b_list = [np.ones(2)]
     for i in range(len(evidence) - 1,-1,-1):
         e = evidence[i]
@@ -81,6 +86,7 @@ def backward(Tmatrix, Omatrices, evidence):
 
 
 def smooth(Tmatrix, Omatrices, evidence):
+    '''Compute the smoothed distributions of the state varable given new evidence.'''
     fm = forward(Tmatrix, Omatrices, evidence)
     bm = backward(Tmatrix, Omatrices, evidence)
     sv = []
@@ -95,16 +101,14 @@ smoothing = smooth(Tmatrix, Omatrices, evidence)
 print("\nSmoothing: ")
 print(smoothing)
 
+# Plotting:
 x = [i for i in range(0, 6)]
 y = [s[1] for s in smoothing]
 plt.plot(x, y, label = "Smoothing", linestyle = "dashed", linewidth = 3)
 plt.legend()
-plt.show()
+#plt.show()
 
-
-# This is called smoothing
-
-# e)
+## e)
 
 
 start_p = np.array([0.5, 0.5]) # Initial distribution over states
@@ -112,24 +116,48 @@ states = [0, 1] # 0 = 'no fish', 1 = 'fish'
 
 
 def viterbi(Tmatrix, Omatrices, evidence, start_p, states):
-    V = np.zeros((len(states), len(evidence)))
+    '''The Viterbi algorithm, based on pseudocode from Wikipedia.'''
+    T1 = np.zeros((len(states), len(evidence)))
+    T2 = np.zeros((len(states), len(evidence)))
     for s in range(len(states)):
-        V[s,0] = start_p[s] * Omatrices[evidence[0]][s,s]
-
+        T1[s,0] = start_p[s] * Omatrices[evidence[0]][s,s]
+    
     for o in range(1, len(evidence)):
         for s in range(len(states)):
-            k = np.argmax([V[k, o - 1] * Tmatrix[k, s] * Omatrices[evidence[o]][s, s] for k in range(len(states))])
-            V[s, o] = V[k, o  - 1] * Tmatrix[k, s] * Omatrices[evidence[o]][s, s]
-    
+            k = np.argmax([T1[k, o - 1] * Tmatrix[k, s] * Omatrices[evidence[o]][s, s] for k in range(len(states))])
+            T1[s, o] = T1[k, o  - 1] * Tmatrix[k, s] * Omatrices[evidence[o]][s, s]
+            T2[s, o] = k
     best_path  = []
-    print(V)
     for o in range(-1, -(len(evidence) + 1), -1):
-        k = np.argmax(V[:, o])
+        k = np.argmax(T1[:, o])
         best_path.insert(0, states[k])
-    return best_path
+    return best_path, T1, T2
 
-print("Viterbi: ")
-print(viterbi(Tmatrix, Omatrices, evidence, start_p, states))
+best_path, T1, T2 = viterbi(Tmatrix, Omatrices, evidence, start_p, states)
+print(T1)
+print(T2)
+
+for t in range(len(evidence)):
+    e = evidence[:(t+1)]
+    print("\nViterbi for t = " + str(t+1))
+    best_path, T1, T2 = viterbi(Tmatrix, Omatrices, e, start_p, states)
+    print("probs: ")
+    print(T1)
+    print("pointers:")
+    print(T2)
+
+
+# Try umbrella example:
+
+Omatrices = [np.array([[0.8, 0], [0, 0.1]]), np.array([[0.2, 0], [0, 0.9]])] # First: no birds,  Second: birds nearby
+Tmatrix = np.array([[0.7, 0.3],[0.3, 0.7]]) # first row corresponds to Xt = false and second row corresponds to Xt = true
+evidence = [1, 1, 0, 1, 1]
+start_p = np.array([0.5,0.5])
+best_path, T1, T2 = viterbi(Tmatrix, Omatrices, evidence, start_p, states)
+print("\n Umbrella:")
+print(best_path)
+print(T1)
+print(T2)
 
 
 
