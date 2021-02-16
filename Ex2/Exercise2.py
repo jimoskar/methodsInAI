14bd1a2b3df784d4e894a948d987c7945d9ccb5d
Jim Totland <jimtotland@gmail.com>
2021-02-16 09:26:02 +0100

    cleaned up. Ready for delivery

--- Ex2/Exercise2.py
+++ Ex2/Exercise2.py
@@ -106,7 +106,7 @@ x = [i for i in range(0, 6)]
 y = [s[1] for s in smoothing]
 plt.plot(x, y, label = "Smoothing", linestyle = "dashed", linewidth = 3)
 plt.legend()
-#plt.show()
+plt.show()
 
 ## e)
 
@@ -116,7 +116,12 @@ states = [0, 1] # 0 = 'no fish', 1 = 'fish'
 
 
 def viterbi(Tmatrix, Omatrices, evidence, start_p, states):
-    '''The Viterbi algorithm, based on pseudocode from Wikipedia.'''
+    '''The Viterbi algorithm, based on pseudocode from Wikipedia.
+       Return the best path and  two tables:
+       T1 contains an unscaled version of m1:t from the book
+       T2 contains pointers to the most likely previous state for each state
+       REMEMBER: row 1 corresponds to Xt = true.
+    '''
     T1 = np.zeros((len(states), len(evidence)))
     T2 = np.zeros((len(states), len(evidence)))
     for s in range(len(states)):
@@ -133,9 +138,6 @@ def viterbi(Tmatrix, Omatrices, evidence, start_p, states):
         best_path.insert(0, states[k])
     return best_path, T1, T2
 
-best_path, T1, T2 = viterbi(Tmatrix, Omatrices, evidence, start_p, states)
-print(T1)
-print(T2)
 
 for t in range(len(evidence)):
     e = evidence[:(t+1)]
@@ -159,66 +161,5 @@ print(best_path)
 print(T1)
 print(T2)
 
+# Seems to give the correct results!
 
-
-def dptable(V):
-    # Print a table of steps from dictionary
-    yield " ".join(("%12d" % i) for i in range(len(V)))
-    for state in V[0]:
-        yield "%.7s: " % state + " ".join("%.7s" % ("%f" % v[state]["prob"]) for v in V)
-
-
-obs = ("birds", "birds", "noBirds", "birds", "birds", "noBirds")
-states = ("fish", "noFish")
-start_p = {"fish": 0.5, "noFish": 0.5}
-trans_p = {
-    "fish": {"fish": 0.8, "noFish": 0.2},
-    "noFish": {"fish": 0.3, "noFish": 0.7},
-}
-emit_p = {
-    "fish": {"birds": 0.75, "noBirds": 0.25},
-    "noFish": {"birds": 0.2, "noBirds": 0.8},
-}
-
-def viterbi(obs, states, start_p, trans_p, emit_p):
-    V = [{}]
-    for st in states:
-        V[0][st] = {"prob": start_p[st] * emit_p[st][obs[0]], "prev": None}
-    # Run Viterbi when t > 0
-    for t in range(1, len(obs)):
-        V.append({})
-        for st in states:
-            max_tr_prob = V[t - 1][states[0]]["prob"] * trans_p[states[0]][st]
-            prev_st_selected = states[0]
-            for prev_st in states[1:]:
-                tr_prob = V[t - 1][prev_st]["prob"] * trans_p[prev_st][st]
-                if tr_prob > max_tr_prob:
-                    max_tr_prob = tr_prob
-                    prev_st_selected = prev_st
-
-            max_prob = max_tr_prob * emit_p[st][obs[t]]
-            V[t][st] = {"prob": max_prob, "prev": prev_st_selected}
-
-    for line in dptable(V):
-        print(line)
-
-    opt = []
-    max_prob = 0.0
-    best_st = None
-    # Get most probable state and its backtrack
-    for st, data in V[-1].items():
-        if data["prob"] > max_prob:
-            max_prob = data["prob"]
-            best_st = st
-    opt.append(best_st)
-    previous = best_st
-
-    # Follow the backtrack till the first observation
-    for t in range(len(V) - 2, -1, -1):
-        opt.insert(0, V[t + 1][previous]["prev"])
-        previous = V[t + 1][previous]["prev"]
-
-    print ("The steps of states are " + " ".join(opt) + " with highest probability of %s" % max_prob)
-
-
-viterbi(obs, states, start_p, trans_p, emit_p)
